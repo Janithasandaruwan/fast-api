4a4ef360880479f33232fd5384ef4f001fdc3f80
janitha <jsjweerasinghe@gmail.com>
2022-01-23 10:13:25 +0530

    remove

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .config import Settings
 from fastapi.middleware.cors import CORSMiddleware
 
 # Create engine
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
